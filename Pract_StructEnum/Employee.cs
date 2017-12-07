00d5897f11040ec12ffe34f60c67730277b32eb9
JekaLion <djoni-01@mail.ru>
2017-12-07 16:16:52 +0600

    init

---
+++ Pract_StructEnum/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract_StructEnum
+{
+    public struct Employee
+    {
+        public string Name { get; set; }
+        public Vacancies Vacanci { get; set; }
+        public int Sallary { get; set; }
+        public int[] EmploymentDate { get; set; }
+
+        public Employee(string name, Vacancies vacanci, int sallary, int[] employmentDate)
+        {
+            Name = name;
+            Vacanci = vacanci;
+            Sallary = sallary;
+            EmploymentDate = new int[3];
+            for (int i = 0; i < EmploymentDate.Length; i++)
+            {
+                EmploymentDate[i] = employmentDate[i];
+            }
+        }
+    }
+}
---
+++ Pract_StructEnum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract_StructEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayDate = new int[3];
+            arrayDate[0] = 21;
+            arrayDate[1] = 11;
+            arrayDate[2] = 12;
+            Employee person = new Employee("LEHA", Vacancies.Manager, 2000, arrayDate);
+            Console.WriteLine("Name; " + person.Name + " Vacancies; " + person.Vacanci + " Sallary; " + person.Sallary + " Employment date; " + person.EmploymentDate[0] + "." +  person.EmploymentDate[1] + "." + person.EmploymentDate[2]);
+            Console.ReadLine();
+        }
+    }
+}
